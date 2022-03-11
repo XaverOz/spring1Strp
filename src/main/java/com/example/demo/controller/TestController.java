c0c073e10e1c21d1533fba7985b3a1ee5d8d205c
zaharov.anatoliy <zaharov.anatoliy@otr.ru>
2022-03-11 09:46:48 +0500

    fix error

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,16 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 
     @GetMapping("/test")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
-        model.addAttribute("text", "123");
+        model.addAttribute("message", name);
         return "test_page";
     }
+
 }
